1e9d7ab06ba857a2d6a77f227119de452bcc3bb8
Hans-J. Schmid <knock@myopendoor.de>
2024-10-20 18:01:10 +0200

    maybe
    $ spago install maybe

--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,12 @@ module Main where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
+
+add :: Int -> Int -> Maybe Int
+add x y = Just (x + y)
 
 main :: Effect Unit
 main = do
