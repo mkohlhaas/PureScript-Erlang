7e3b9ebd3cee58f6efc6dc604ebef9b81ec8ecaf
Hans-J. Schmid <knock@myopendoor.de>
2024-10-21 20:03:17 +0200

    reset Main.purs

--- src/Main.purs
+++ src/Main.purs
@@ -2,14 +2,9 @@ module Main where
 
 import Prelude
 
-import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
-
-add1 :: Int -> Int -> Maybe Int
-add1 x y = Just (x + y)
 
 main :: Effect Unit
 main = do
   log "🍝"
-  log $ show $ add1 5 7
